ffc95e39bcdd8162754d5c1754acc4a9d7dad756
maximoffminis <102333265+maximoffminis@users.noreply.github.com>
2022-06-28 19:35:57 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -21,7 +21,7 @@ function setup() {
   var option={
     isStatic:true
   };
-  ground=Bodies.rectangle(100,300,400,20);
+  ground=Bodies.rectangle(100,300,400,20,option);
   World.add(world,ground);
   //box1 = new Box(200,100,50,50);
   
@@ -33,7 +33,7 @@ function mousePressed(){
   boxes.push(
     
     
-    new Box(50,50,mouseX,mouseY)
+    new Box(mouseX,mouseY, 50,50)
     
     
     )
